ceac080a7978ee9e9f5a6537a0dc283696ee2eea
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-05-04 07:20:18 -0500

    v 0.5
    Login y registro terminados

--- assets/js/Auth.js
+++ assets/js/Auth.js
@@ -1,3 +1,32 @@
 function Login() {
-    alert("clicked");
+    var credenciales;
+    let correo, contraseña, contraseña_revisar;
+    correo = document.getElementById("email").value;
+    contraseña_revisar = document.getElementById("password").value;
+    /* Checking if the browser supports local storage. */
+    if(typeof(Storage)!="undefined"){
+        /* Checking if the user exists in the local storage. */
+        if(localStorage.getItem(correo)!=undefined){
+            
+            /* Getting the information of the user from the local storage. */
+            credenciales = JSON.parse(localStorage.getItem(correo));
+            /* For debug
+            console.log(credenciales);
+            alert("entro");
+            */
+
+            /* Checking if the password is correct. */
+            if(credenciales.contraseña == md5(contraseña_revisar)){
+                alert("Bienvenido, "+credenciales.nombre);
+                sessionStorage.setItem("aux", true); //Si aux es true significa que el usuario esta logeado   
+            }
+        }else{
+            alert("Usuario no existe");
+        }
+    }else{
+        alert("El navegador no soporta localstorage")
+    }
+
+
+
 }
